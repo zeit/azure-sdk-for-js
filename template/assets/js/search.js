b00b242e86bff4d17884ae4abf0297b9cd8aa310
Azure SDK Builder <{ID}+{username}@users.noreply.github.com>
2019-09-27 04:33:52 +0000

    Auto-generated docs from azure-sdk-for-js SHA 25cecf1a71becff5b214f2c9407a1fc8366e567c and SHA f0066851f2889e72c2348317a1b41696ca9a6acd

---
+++ template/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"default","url":"globals.html#default","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"createEventEmitter","url":"globals.html#createeventemitter","classes":"tsd-kind-function"}]};
